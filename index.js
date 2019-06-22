1c0911550f37ea4251ed9caac5cbb103f905c748
moniet <moniet.sawhney@gmail.com>
2019-06-22 18:24:39 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+wrapGifts(gifts);
+
+function writeCards(namesArr, event) {
+  var cards = [];
+
+  for (let i = 0; i < namesArr.length; i++) {
+    let message = `Thank you, ${namesArr[i]}, for the wonderful ${event} gift!`;
+    cards.push(message);
+  }
+
+  return cards;
+}
+
+function countdown(num) {
+  while (num > 0) {
+    console.log(num);
+    num -= 1;
+  }
+
+  console.log(num);
+}
